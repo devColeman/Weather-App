62b49e8c02ea6ab3c25b4414e9da17b09de238f2
Devin Coleman <croilders48@gmail.com>
2025-02-23 00:42:36 -0800

    added imgs and some more text

--- main.js
+++ main.js
@@ -4,13 +4,19 @@ import {
 
 document.querySelector('button').addEventListener('click', getData)
 let input = document.getElementById('search')
+const image = document.getElementById('myImage');
 
+
+// todo:
+// add city names, temperature hunidity wind speed and add weather icons
+// watch the yt videon and look up some weather apps to use ideas from
 
 getData()
 
 
 
 async function getData() {
+  
     let url = ``
     if(input.value === ''){
          url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=London`
@@ -32,9 +38,13 @@ async function getData() {
       console.log(data)
       let currentTemp = Math.round(data.current.temp_f)
       let feelsLike = Math.round(data.current.feelslike_f)
+      let imgHeader = data.current.condition.text
+      let img = data.current.condition.icon
+
+      image.src = img;
       document.querySelector('h2').innerHTML = `Temperatue is ${currentTemp} °F`
       document.querySelector('h3').innerHTML = `Feels like ${feelsLike} °F`
-      document.querySelector('h4').innerHTML = ''
+      document.querySelector('h4').innerHTML = imgHeader
       
     } catch (error) {
       console.error(error.message);
