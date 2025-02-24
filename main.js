a0f7c575e31d9dc39505bddb15f0454a2624f7bc
Devin Coleman <croilders48@gmail.com>
2025-02-24 06:00:43 -0800

    cleaned some things up

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ const image = document.getElementById('myImage');
 
 
 // todos:
-// change placeholder on search to include both place and country
 // add city names, temperature hunidity wind speed and add weather icons
 // watch the yt videon and look up some weather apps to use ideas from
 // add pop out for 3 or 5 day forecast
+// make the climate condition its own box with a box below that has the 5 day weather forecast
 
 getData()
 
@@ -37,17 +37,43 @@ async function getData() {
       }
       
       const data2 = await response.json();
-      console.log(data2)
+      
 
       // filters out the 0 from times ex: 06:24 am => 6:24 am using function removeZero
       let f = data2.astronomy.astro.sunrise.split('')
-       document.querySelector('.sunrise').innerHTML = ` ${removeZero(f)}`
+       document.querySelector('.sunrise').innerHTML = ` ${removeZero(f)} `
 
       let z = data2.astronomy.astro.sunset.split('')
       document.querySelector('.sunset').innerHTML = `${removeZero(z)}`
 
       let k = data2.astronomy.astro.moonrise.split('')
       document.querySelector('.moonrise').innerHTML = ` ${removeZero(k)}`
+
+      // adding the moon pic
+      let moonStuff = data2.astronomy.astro.moon_phase
+      let moonPhase = moonStuff.toLowerCase()
+    
+       
+      if(moonPhase == 'new moon'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌑`
+      } else if (moonPhase == 'waxing cresent'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌒`
+      } else if (moonPhase == 'first quarter'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌓`
+      } else if (moonPhase == 'waxing gibbous'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌔`
+      } else if (moonPhase == 'full moon'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌔`
+      } else if (moonPhase == 'waning gibbous'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌖`
+      } else if (moonPhase == 'last quarter'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌗`
+      } else if (moonPhase == 'waning crescent'){
+        document.querySelector('.moonPhase').innerHTML = `${data2.astronomy.astro.moon_phase} 🌘`
+      } 
+      
+
+
 
     
       
@@ -67,7 +93,7 @@ async function getData() {
       }
   
       const data = await response.json();
-      console.log(data)
+      // console.log(data)
       let currentTemp = Math.round(data.current.temp_f)
       let feelsLike = Math.round(data.current.feelslike_f)
       let imgHeader = data.current.condition.text
@@ -81,19 +107,24 @@ async function getData() {
       document.querySelector('.currentTemp').innerHTML = `${currentTemp}°F / Feels like ${feelsLike}°F`
       document.querySelector('.imgHeader').innerHTML = imgHeader
       document.querySelector('.location').innerHTML = data.location.name
+      let locationcountry = ''
       
       if( data.location.name === data.location.region){
         document.querySelector('.location').innerHTML = `${data.location.name} / ${data.location.country} `
+        locationcountry = data.location.country
       } else if(data.location.country === "United States of America") {
         document.querySelector('.location').innerHTML = `${data.location.name} / ${data.location.region
         } `
+        locationcountry = data.location.region
       } else {
         document.querySelector('.location').innerHTML = `${data.location.name} / ${data.location.country
         } `
+        locationcountry = data.location.country
       }
       
+        document.querySelector('.humidity').innerHTML = `Humidity: ${data.current.humidity}%`
      
-      input.placeholder = `${data.location.name}`
+      input.placeholder = `${data.location.name}, ${locationcountry}`
       input.value = ''
       
     } catch (error) {
